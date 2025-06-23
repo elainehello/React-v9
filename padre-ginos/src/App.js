9580ed397b22a8496a553527a10a1ab5a024cf77
elaine <195331169+elainehello@users.noreply.github.com>
2025-06-23 10:35:16 +0200

    typo fixed

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,4 +1,4 @@
-const Pizza = (p) => {
+const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("p", {}, props.description),

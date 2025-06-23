484d3806c96de08e0f538cb5b367cb3d662dcd20
elaine <195331169+elainehello@users.noreply.github.com>
2025-06-23 16:17:02 +0200

    react config | js to jsx changes

--- padre-ginos/eslint.config.mjs
+++ padre-ginos/eslint.config.mjs
@@ -1,10 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-pluging-react";
 
 /**@type {import('eslint').Linter.Config[]} */
 export default [
-    js.configs.recommended,
+    js.configs.recommended, {
+        ...reactPlugin.configs.flat.recommended,
+        settings: {
+            react: {
+                version: "dectect",
+            },
+        },
+    },
+    reactPlugin.configs.flat["jsx-runtime"],
     {
         files: ["**/*.js", "**/*.jsx"],
         languageOptions: {
@@ -15,7 +24,10 @@ export default [
                 },
             },
         },
-
+        rules: {
+            "react/no-unescaped-entitites": "off",
+            "react/prop-types": "off",
+        },
     },
     prettier,
 ];

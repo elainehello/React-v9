b63e0b12b6268d8e6ee7dfb0cd62be26674d1c31
elaine <195331169+elainehello@users.noreply.github.com>
2025-06-26 01:07:03 +0200

    typo fixed

--- padre-ginos/vite.config.js
+++ padre-ginos/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from '@tanstack/react-plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
     server: {

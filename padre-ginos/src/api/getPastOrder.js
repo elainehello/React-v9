95fcd5731995e6579fdd3a882901eaeaf2fda666
elaine <195331169+elainehello@users.noreply.github.com>
2025-06-26 17:46:15 +0200

    promise

--- padre-ginos/src/api/getPastOrder.js
+++ padre-ginos/src/api/getPastOrder.js
@@ -1,4 +1,5 @@
 export default async function getPastOrder(order) {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
     const response = await fetch(`/api/past-order/${order}`);
     const data = await response.json();
     return data
